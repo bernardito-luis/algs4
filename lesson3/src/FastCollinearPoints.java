5b1b9bdcd5a491d0b263082fbed64b3cf2dc62bd
Max Belkin <bernardito.luis.el.gorra@gmail.com>
2017-03-15 16:15:14 +0300

    Bug fixes #7.

--- lesson3/src/FastCollinearPoints.java
+++ lesson3/src/FastCollinearPoints.java
@@ -1,3 +1,7 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -70,9 +74,42 @@ public class FastCollinearPoints {
 
     public LineSegment[] segments() {
         LineSegment[] array = new LineSegment[segments.size()];
-        return segments.toArray(array);
+        for (int i = 0; i < segments.size(); i++) {
+            array[i] = segments.get(i);
+        }
+        return array;
     }
 
     public static void main(String[] args) {
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+        System.out.println(collinear.numberOfSegments());
+        LineSegment[] segms = collinear.segments();
+        System.out.println(segms);
+        System.out.println("Success!!");
     }
 }
